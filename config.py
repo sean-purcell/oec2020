37dae0f4ee200363de25cc6e9c0b17ac77dae587
Celine O'Neil <celineloneil@gmail.com>
2020-01-17 22:48:44 -0500

    Add consumer rate input to optimizer

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
+
+from enum import Enum
+
 # Constants
-
-SUMMER_TEMPS = []
-
 EMISSIONS = {
         'nuclear' : 6,
         'solar': 105,
@@ -21,3 +21,23 @@ PRICES = {
         'biofuel' : 131,
         'buyable' : 160,
 }
+
+class Season(Enum):
+        SUMMER = 0
+        WINTER = 1
+
+def get_season(inrow):
+        if sum(inrow.temps) / len(inrow.temps) > 10:
+                return Season.SUMMER
+        return Season.WINTER
+
+# blocks of time include start and exclude end
+def consumer_rate(season, time):
+        if time >= 21 or time < 7:  # off-peak, 21:00 - 7:00
+                return 6.5
+        elif time < 11 or time >= 17:  # 7:00 - 11:00 or 17:00 - 21:00
+                return 9.4 if season == Season.SUMMER else 13.4
+
+        # 11:00 - 17:00
+        return 13.4 if season == Season.SUMMER else 9.4
+
--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import sys
 
+import config
 import parse
 import optimizer
 
 def main():
     (init, hours) = parse.parse_csv(open(sys.argv[1], 'r'))
 
+    season = config.get_season(hours[0])
     nuclear = init[-1].mw_drawn.nuclear
 
     for hour in hours:
         print(hour.time)
+        rate = config.consumer_rate(season, hour.time)
         print('Cost:')
-        print(optimizer.optimize(hour, nuclear, {'cost': -1, 'co2': 0, 'green': 0}, debug=False))
+        print(optimizer.optimize(hour, rate, nuclear, {'cost': -1, 'co2': 0, 'green': 0}, debug=False))
         print('Co2:')
-        print(optimizer.optimize(hour, nuclear, {'cost': 0, 'co2': -1, 'green': 0}, debug=False))
+        print(optimizer.optimize(hour, rate, nuclear, {'cost': 0, 'co2': -1, 'green': 0}, debug=False))
         print('Green:')
-        print(optimizer.optimize(hour, nuclear, {'cost': 0, 'co2': 0, 'green': 1}, debug=False))
+        print(optimizer.optimize(hour, rate, nuclear, {'cost': 0, 'co2': 0, 'green': 1}, debug=False))
 
 if __name__ == '__main__':
     main()
